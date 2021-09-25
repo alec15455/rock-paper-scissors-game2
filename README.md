35e577e76646a5e331a18bd12730da92088a0fc7
alec15455 <89539218+alec15455@users.noreply.github.com>
2021-09-25 19:26:48 -0400

    e

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # rock-paper-scissors-game2
+
+# After you download my file onto your desktop, open it through the external editor. Then, open the file in terminal. Make sure you're in the correct file by typing "pwd"
+# After this, type "conda create -n my-game-env python=3.8 # (first time only) or conda activate my-game-env"
+# After, to download the required files, type "pip-install -r requirements.txt"
+# Then, you're ready to play the game. Type "python game.py"
+# Type your choice and you'll see the result
+# To play again type "python game.py" again
--- game.py
+++ game.py
@@ -1,6 +1,10 @@
 # game.py
 
 import random
+from dotenv import load_dotenv
+
+load_dotenv()
+user_name = os.getenv("PLAYER_NAME", default = "default")
 
 print("Rock, Paper, Scissors, Shoot!")
 
@@ -25,18 +29,25 @@ tie = "TIE!"
 
 if (computer_choice == user_choice) :
     result = tie
+        print ("TIE!")
 elif ((user_choice == "rock") and (computer_choice == "scissors")) :
     result = user_wins
+        print("YOU WIN!")
 elif ((user_choice == "scissors") and (computer_choice == "rock")) :
     result = computer_wins
+        print("YOU LOSE!")
 elif ((user_choice == "rock") and (computer_choice == "paper")) :
     result = user_wins
+        print("YOU WIN!")
 elif ((user_choice == "paper") and (computer_choice == "rock")) :
     result = computer_wins
+        print("YOU LOSE!")
 elif ((user_choice == "scissors") and (computer_choice == "paper")) :
     result = user_wins
+        print("YOU WIN!")
 elif ((user_choice == "paper") and (computer_choice == "scissors")) :
     result = computer_wins
-
-print(result)
-print("THANKS PLEASE PLAY AGAIN")
+        print("YOU LOSE!")
+else:
+    print("Please enter rock, paper, or scissors")
+exit()
