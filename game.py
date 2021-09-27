f70b12561c24078c25b1369764fac61d7840f6eb
alec15455 <89539218+alec15455@users.noreply.github.com>
2021-09-26 20:30:09 -0400

    Update game.py

--- game.py
+++ game.py
@@ -24,17 +24,13 @@ if user_choice == computer_choice:
     print ("TIE!")
 
 if user_choice == "rock":
-    if computer_choice == "rock":
-        print("YOU LOSE!")
-    elif computer_choice == "paper":
+    if computer_choice == "paper":
         print("YOU LOSE!")
     elif computer_choice == "scissors":
         print("YOU WIN!")
 elif user_choice == "paper":
     if computer_choice == "rock":
         print("YOU WIN!")
-    elif computer_choice == "paper":
-        print("YOU LOSE!")
     elif computer_choice == "scissors":
         print("YOU LOSE!")
 elif user_choice == "scissors":
@@ -42,8 +38,6 @@ elif user_choice == "scissors":
         print("YOU LOSE!")
     elif computer_choice == "paper":
         print("YOU WIN!")
-    elif computer_choice == "scissors":
-        print("YOU LOSE!")
 else:
     print("Please enter rock, paper, or scissors!")
 exit()
